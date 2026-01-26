b8ca341fa4b9040faf638456e7f82dedca4e03f3
Francisco Junior <fgsj.dev@outlook.com>
2026-01-25 21:43:44 -0300

    Exercise: Counter regressive

---
+++ learnJava/src/Repetition/Exercise02.java
@@ -0,0 +1,9 @@
+package Repetition;
+
+public class Exercise02 {
+    public static void main(String[] args) {
+        for (int i = 50; i >= 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
