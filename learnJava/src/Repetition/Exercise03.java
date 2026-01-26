50a1241d1a971c5fbcb8f334618a3c6ed5a2814b
Francisco Junior <fgsj.dev@outlook.com>
2026-01-25 21:46:00 -0300

    Exercise: sum of numbers in interval

---
+++ learnJava/src/Repetition/Exercise03.java
@@ -0,0 +1,12 @@
+package Repetition;
+
+public class Exercise03 {
+    public static void main(String[] args) {
+        Integer sum=0;
+
+        for (int i = 1; i <=100 ; i++) {
+            sum += i;
+        }
+        System.out.printf("The sum of numbers: %d", sum);
+    }
+}
