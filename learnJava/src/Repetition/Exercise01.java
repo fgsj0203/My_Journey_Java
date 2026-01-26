fa925ff0e414af83192b3ae4d7e77f46237a581a
Francisco Junior <fgsj.dev@outlook.com>
2026-01-25 21:33:43 -0300

    Exercise: printing number 1-50

---
+++ learnJava/src/Repetition/Exercise01.java
@@ -0,0 +1,9 @@
+package Repetition;
+
+public class Exercise01 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 50 ; i++) {
+            System.out.println(i);
+        }
+    }
+}
